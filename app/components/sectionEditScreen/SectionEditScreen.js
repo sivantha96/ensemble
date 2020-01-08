7ebe4de668b5d16660e576442f51c4383f457f9a
Sivantha Paranavithana <madusham3@gmail.com>
2020-01-08 08:03:42 +0530

    Updated SongView Tempelate

--- app/components/sectionEditScreen/SectionEditScreen.js
+++ app/components/sectionEditScreen/SectionEditScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { 
     Text, 
-    StyleSheet, 
-    View, 
-    Button
+    View,
+    Button,
+    TextInput,
+    Image,
 } from 'react-native'
+import styles from './Styles'
 
 export default class SectionEditScreen extends Component {
     //constructor to hold the information in the state
@@ -26,11 +28,42 @@ export default class SectionEditScreen extends Component {
 
     render() {
         return (
-            <View>
-                <Button title='add chorus' onPress={() => this.props.navigation.navigate('NewSong', { newSection: this.state.section})}/>
+            <View style={styles.appContainer}>
+                <View style={styles.topContainer}>
+                    <View style={styles.infoContainer}>
+                        <View style={styles.titleContainer}>
+                        <TextInput style={styles.titleInput}
+                            enablesReturnKeyAutomatically = {true}
+                            keyboardAppearance= 'dark'
+                            returnKeyType= 'done'
+                            placeholder= 'Untitled Section'
+                            placeholderTextColor= '#fff'
+                            onChangeText = {(text) => this.setState({title: text})}
+                            />
+                        </View>
+                        <View style={styles.manyButtonContainer}>
+                            <View style={styles.singleButtonContainer}>
+                                <Button title='add chorus' onPress={() => this.props.navigation.navigate('NewSong', { newSection: this.state.section})}/>
+                            </View>
+                            <View style={styles.singleButtonContainer}>
+                                
+                            </View>
+                            <View style={styles.singleButtonContainer}>
+                                
+                            </View>
+                            <View style={styles.singleButtonContainer}>
+
+                            </View>
+                            <View style={styles.singleButtonContainer}>
+
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.bottomContainer }>
+
+                </View>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({})
